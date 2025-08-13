34482f6707825b2e1b61c662ad62bbb17752a28a
malli <mallikarjuna2314@gmail.com>
2025-08-14 00:48:00 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -1,22 +1,55 @@
 #!/bin/bash
 
-AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-01bc7ebe005fb1cb2" # replace with your SG ID
-INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
-ZONE_ID="Z032558618100M4EJX8X4" # replace with your ZONE ID
-DOMAIN_NAME="daws84s.site" # replace with your domain
+# AMI_ID="ami-09c813fb71547fc4f"
+# SG_ID="sg-00d233e7d20a6b130"
+# INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "cart" "user" "shipping" "payment" "dispatch" "frontend")
+# ZONE_ID="Z085956734PDTWQYOZGJR"
+# DOMAIN_NAME="malli12.site"
 
-#for instance in ${INSTANCES[@]}
-for instance in $@
+# for instance in ${INSTANCES[@]}
+# do
+#     #INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-00d233e7d20a6b130 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+#     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-00d233e7d20a6b130 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text) 
+#      if [ $instance != "frontend" ]
+#     then
+#         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    
+#     else
+#         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+     
+#     fi
+#     echo "$instance IP address: $IP"
+# done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-00d233e7d20a6b130" # replace with your SG ID
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z085956734PDTWQYOZGJR" # replace with your ZONE ID
+DOMAIN_NAME="malli12.site" # replace with your domain
+
+for instance in ${INSTANCES[@]}
+
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01bc7ebe005fb1cb2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-00d233e7d20a6b130 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-        RECORD_NAME="$instance.$DOMAIN_NAME"
+        
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
-        RECORD_NAME="$DOMAIN_NAME"
     fi
     echo "$instance IP address: $IP"
 
@@ -28,7 +61,7 @@ do
         ,"Changes": [{
         "Action"              : "UPSERT"
         ,"ResourceRecordSet"  : {
-            "Name"              : "'$RECORD_NAME'"
+            "Name"              : "'$instance'.'$DOMAIN_NAME'"
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
@@ -37,4 +70,67 @@ do
         }
         }]
     }'
+
 done
+
+ 
+
+
+
+# AMI_ID="ami-09c813fb71547fc4f"
+# SG_ID="sg-00d233e7d20a6b130"
+# INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "cart" "user" "shipping" "payment" "dispatch" "frontend")
+# ZONE_ID="Z0839647F7C8RDZW5D3P"
+# DOMAIN_NAME="malli12.site"
+
+# for instance in ${INSTANCES[@]}
+# do
+#     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-00d233e7d20a6b130 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+#      if [ $instance -ne "frontend" ]
+#     then
+#         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+#     else
+#         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+     
+#     fi
+#     echo "$instance IP address: $IP"
+# done
+
+
+# AMI_ID="ami-09c813fb71547fc4f"
+# SG_ID="sg-00d233e7d20a6b130" # replace with your SG ID
+# INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+# ZONE_ID="Z0839647F7C8RDZW5D3P" # replace with your ZONE ID
+# DOMAIN_NAME="malli12.site" # replace with your domain
+# #for instance in ${INSTANCES[@]}
+# for instance in ${INSTANCES[@]}
+# do
+#     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-00d233e7d20a6b130 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+#     if [ $instance != "frontend" ]
+#     then
+#         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+#         RECORD_NAME="$instance.$DOMAIN_NAME"
+#     else
+#         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+#         RECORD_NAME="$DOMAIN_NAME"
+#     fi
+#     echo "$instance IP address: $IP"
+
+#     aws route53 change-resource-record-sets \
+#     --hosted-zone-id $ZONE_ID \
+#     --change-batch '
+#     {
+#         "Comment": "Creating or Updating a record set for cognito endpoint"
+#         ,"Changes": [{
+#         "Action"              : "UPSERT"
+#         ,"ResourceRecordSet"  : {
+#             "Name"              : "'$RECORD_NAME'"
+#             ,"Type"             : "A"
+#             ,"TTL"              : 1
+#             ,"ResourceRecords"  : [{
+#                 "Value"         : "'$IP'"
+#             }]
+#         }
+#         }]
+#     }'
+# done
